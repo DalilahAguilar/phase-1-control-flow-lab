19ec882d3f7bb0487497da3ed10cf8c479e55aa8
Dalilah Aguilar <dalilahaguilar9@gmail.com>
2021-08-30 23:19:48 -0500

    commit tree 8eb7b695d9fd37c559cb4cffdfe98b74dbf8d318

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(x) {
+if (x <= 400){
+return "This one is on me!";
+} else if (x > 2000 && x < 2500){
+return 'I will gladly take your thirty bucks.';
+} else if (x >= 2500);
+return 'No can do.'
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+if (city === 'NYC') {
+    return 'Ok, sounds good.'
+} else 
+return 'No go.'
 }
